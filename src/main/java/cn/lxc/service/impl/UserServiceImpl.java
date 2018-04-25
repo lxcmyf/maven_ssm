0e0b3b95feedb9d606b3da7501e72bb36bff34ca
lxcmyf <lxcmyf@qq.com>
2018-04-25 13:20:29 +0800

    GX

--- src/main/java/cn/lxc/service/impl/UserServiceImpl.java
+++ src/main/java/cn/lxc/service/impl/UserServiceImpl.java
@@ -5,38 +5,34 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import cn.lxc.dao.UserDao;
+import cn.lxc.mapper.UserMapper;
 import cn.lxc.service.UserService;
 import cn.lxc.vo.User;
 
 @Service("userService")
 public class UserServiceImpl implements UserService{
 	@Autowired
-	private UserDao userDao;
+	private UserMapper userMapper;
 	
 	@Override
 	public List<User> getList() {
-		return userDao.getList();
+		return userMapper.getList();
 	}
 	@Override
 	public int delete(int id) {
-		return userDao.delete(id);
+		return userMapper.delete(id);
 	}
 	@Override
 	public int save(User user) {
-		return userDao.save(user);
+		return userMapper.save(user);
 		
 	}
 	@Override
 	public User showInfoById(int id) {
-		return userDao.getEntity(id);
+		return userMapper.getEntity(id);
 	}
 	@Override
 	public int update(User user) {
-		return userDao.update(user);
-	}
-	/*************************getter&setter**************************/
-	public void setUserDao(UserDao userDao) {
-		this.userDao = userDao;
+		return userMapper.update(user);
 	}
 }
